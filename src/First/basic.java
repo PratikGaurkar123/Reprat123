aece1698cd200273df09cb4afb1ec74dad5eab55
ASUS <ASUS@LAPTOP-06IB4115>
2021-06-25 21:25:58 +0530

    [Pratik]:commited 2test

--- src/First/basic.java
+++ src/First/basic.java
@@ -5,6 +5,6 @@ public class basic {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Github");
-		System.out.println("got file of github ");
+		System.out.println("got file of github1 ");
 	}
 }
